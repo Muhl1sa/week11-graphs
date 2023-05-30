ff59fe33d74090a9a3e9bbaec00f99b90810f12e
Muhl1sa <114937104+Muhl1sa@users.noreply.github.com>
2023-05-30 20:23:49 +0500

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -4,8 +4,36 @@ using namespace std;
 
 vector<vector<int> > solve(vector<vector<int> >& image, int sr, int sc, int color) {
 
-    //completer the function
+    
     int n = image.size();
+    int m = image[0].size();
+     if (sr < 0 || sr >= n || sc < 0 || sc >= m || image[sr][sc] != originalColor)
+        return;
+
+   
+    image[sr][sc] = color;
+
+  
+    floodFill(image, sr - 1, sc, color, originalColor); 
+    floodFill(image, sr + 1, sc, color, originalColor);
+    floodFill(image, sr, sc - 1, color, originalColor);
+    floodFill(image, sr, sc + 1, color, originalColor); 
+}
+
+vector<vector<int>> solve(vector<vector<int>>& image, int sr, int sc, int color) {
+   
+    int originalColor = image[sr][sc];
+
+    
+    if (originalColor == color)
+        return image;
+
+   
+    floodFill(image, sr, sc, color, originalColor);
+
+    return image;
+}
+
 }
 
 //do not modify the code below
@@ -15,8 +43,8 @@ int main()
     cin >> n;
     vector<vector<int> > image(n, vector<int>(n, 0));
     for(int i = 0; i < n; i ++)
-        for(int j = 0; j < n; j ++)
-            cin >> image[i][j];
+    for(int j = 0; j < n; j ++)
+        cin >> image[i][j];
     int sr, sc, color;
     cin >> sr >> sc >> color;
 
@@ -24,7 +52,7 @@ int main()
     for(int i = 0; i < n; i ++)
     {
         for(int j = 0; j < n; j ++)
-        {   
+        {  
             cout << ans[i][j];
             if(j != n - 1)
                 cout << ' ';
