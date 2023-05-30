f7c059abec4833dffbe696dbeea0fdf6ab15fa3b
Muhl1sa <114937104+Muhl1sa@users.noreply.github.com>
2023-05-30 20:45:10 +0500

    Update problem_2.cpp

--- problem_2.cpp
+++ problem_2.cpp
@@ -3,8 +3,49 @@
 using namespace std;
 
 int solve(int n, vector<vector<int> >& edges, int distanceThreshold) {
+   vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+
     
-  //complete the function
+    for (int i = 0; i < n; i++)
+        dist[i][i] = 0;
+
+    
+    for (const vector<int>& edge : edges) {
+        int from = edge[0];
+        int to = edge[1];
+        int weight = edge[2];
+        dist[from][to] = weight;
+        dist[to][from] = weight;
+    }
+
+    
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    int minCities = INT_MAX;
+    int minCity = -1;
+
+  
+    for (int i = 0; i < n; i++) {
+        int reachableCities = 0;
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] <= distanceThreshold)
+                reachableCities++;
+        }
+        if (reachableCities <= minCities) {
+            minCities = reachableCities;
+            minCity = i;
+        }
+    }
+
+    return minCity;
+}
    
 }
 
